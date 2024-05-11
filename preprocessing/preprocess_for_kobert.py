817a306cabc6acb99dfa13b4e4c46b1fa3e39db7
SanghyeonLee <hyeon012366@gmail.com>
2024-05-11 16:10:00 +0900

    feat: 불용어 리스트 정의 및 전처리 적용

--- preprocessing/preprocess_for_kobert.py
+++ preprocessing/preprocess_for_kobert.py
@@ -12,7 +12,15 @@ def text_preprocessing(text, stopwords):
 
 def main():
     df = pd.read_csv('../data/output.csv', encoding='utf-8-sig')
-    print(df.head())
+
+    # 불용어 리스트 정의
+    stopwords = ['의', '가', '이', '은', '들', '는', '좀', '잘', '걍', '과', '도', '를', '으로', '자', '에', '와', '한', '하다']
+
+    # 전처리 적용
+    df['content'] = df['content'].apply(lambda x: text_preprocessing(x, stopwords))
+    df['ocr_data'] = df['ocr_data'].apply(lambda x: text_preprocessing(x, stopwords))
+
+    print(df[['content', 'ocr_data']].head())
 
 if __name__ == '__main__':
     main()
