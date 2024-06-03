aa07c6ea60fdda83d616c6c49c432578ca3c7d3a
SanghyeonLee <hyeon012366@gmail.com>
2024-06-03 19:12:00 +0900

    rename: 불필요 코드 제거

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,10 +3,11 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": "*"}})  # 필요한 도메인만 열어도 OK
-
+    CORS(app, resources={r"/*": {"origins": "*"}})
+  
     from app.routes.blog import blog_bp
     from app.routes.process import process_bp
+
 
     app.register_blueprint(blog_bp)
     app.register_blueprint(process_bp)
