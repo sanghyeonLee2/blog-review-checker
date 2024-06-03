97400886cb0c8e848027c58b1e149b7f9181a5ef
SanghyeonLee <hyeon012366@gmail.com>
2024-06-03 19:11:00 +0900

    fix: 경로 문제 수정

--- server/run.py
+++ server/run.py
@@ -1,9 +1,20 @@
-from app import create_app
+import sys
+import os
 
-app = create_app()
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.append(current_dir)
+
+from app import create_app
 
 if __name__ == '__main__':
     import ssl
+
+    app = create_app()
+
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    context.load_cert_chain('ssl/cert.pem', 'ssl/key.pem')
-    app.run(host='0.0.0.0', port=3000, ssl_context=context)
+    context.load_cert_chain(
+        os.path.join(current_dir, 'app', 'ssl', 'cert.pem'),
+        os.path.join(current_dir, 'app', 'ssl', 'key.pem')
+    )
+    app.run(host='0.0.0.0', port=3000, debug=True, ssl_context=context)
