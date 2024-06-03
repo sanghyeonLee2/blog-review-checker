76af6d7b9d1e8c4047d4e7724bcd9d16b124268f
SanghyeonLee <hyeon012366@gmail.com>
2024-06-03 16:11:00 +0900

    chore: 크롬 익스텐션에서 모듈 방식으로 실행 안됨으로 파일 제거

--- client/api/apis.js
+++
@@ -1,6 +0,0 @@
-import { postRequest } from "./postRequest.js";
-
-export const crawlBlog = (blogUrl) =>
-  postRequest("/blog-crawling", { blogUrl });
-
-export const processBlogContent = (data) => postRequest("/process", data);
